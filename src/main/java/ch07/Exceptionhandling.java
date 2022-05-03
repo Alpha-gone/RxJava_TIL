41a1049e056cd486e2d46bd896fa6381df79e25e
Alpha-gone <990131@naver.com>
2022-05-04 01:08:13 +0900

    22.05.04 - 챕터 7 코드 15까지 완료

--- src/main/java/ch07/Exceptionhandling.java
+++ src/main/java/ch07/Exceptionhandling.java
@@ -3,13 +3,28 @@ package ch07;
 import common.CommonUtils;
 import common.Log;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class Exceptionhandling {
     public static void main(String[] args) {
         Exceptionhandling exceptionhandling = new Exceptionhandling();
+        String[] grades = {"70", "88", "$100", "93", "83"};
 
         CommonUtils.divSection("cannotCatch");
-        exceptionhandling.cannotCatch();
+        //exceptionhandling.cannotCatch();
+
+        CommonUtils.divSection("onErrorReturn");
+        exceptionhandling.onErrorREturn(grades);
+
+        CommonUtils.divSection("onError");
+        exceptionhandling.onError(grades);
+
+        CommonUtils.divSection("onErrorReturnItem");
+        exceptionhandling.onErrorReturnItem(grades);
+
+        CommonUtils.divSection("onErrorResumeNext");
+        exceptionhandling.onErrorResumeNext();
     }
 
     private void cannotCatch(){
@@ -27,5 +42,78 @@ public class Exceptionhandling {
         }catch (Exception e){
             Log.e(e.getMessage());
         }
+    }
+
+    private void onErrorREturn(String[] values){
+        Observable<Integer> observable = Observable.fromArray(values)
+                .map(data -> Integer.parseInt(data))
+                .onErrorReturn(e -> {
+                    if(e instanceof NumberFormatException){
+                        e.printStackTrace();
+                    }
+                    return -1;
+                });
+
+        observable.subscribe(data -> {
+            if(data < 0){
+                Log.e("Wrong Data found!!");
+                return;
+            }
+            Log.i("Grade is " + data);
+            }
+        );
+    }
+
+    private void onError(String[] values){
+        Observable<Integer> observable = Observable.fromArray(values)
+                .map(Integer::parseInt);
+
+        observable.subscribe(data -> Log.i("Grade is " + data),
+                e -> {
+                        if(e instanceof NumberFormatException){
+                            e.printStackTrace();
+                        }
+                        Log.e("Wrong Data found!!");
+                    }
+                );
+    }
+
+    private void onErrorReturnItem(String[] valuse){
+        Observable<Integer> observable = Observable.fromArray(valuse)
+                .map(data -> Integer.parseInt(data))
+                .onErrorReturnItem(-1);
+
+        observable.subscribe(data ->{
+                if(data < 0){
+                    Log.e("Wrong Data found");
+                    return;
+                }
+
+                Log.i("Grade is " + data);
+            }
+        );
+    }
+
+    private void onErrorResumeNext(){
+        String[] salesData = {"100", "200", "A300"};
+        Observable<Integer> onParseError = Observable.defer(() ->{
+            Log.d("send email to administrator");
+            return Observable.just(-1);
+            })
+            .subscribeOn(Schedulers.io());
+
+        Observable<Integer> observable = Observable.fromArray(salesData)
+                .map(Integer::parseInt)
+                .onErrorResumeNext(e -> onParseError);
+
+        observable.subscribe(data -> {
+                if(data < 0){
+                    Log.e("Wrong Data found!!");
+                    return;
+                }
+
+            Log.i("Sales data " + data);
+            }
+        );
     }
 }
